cfb4d56703edd0f95383f71811c7397dd4699908
Sean <sean@codekoalas.com>
2018-01-08 13:29:30 -0600

    Completed the file metadata challenge

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,15 @@
+const fs = require('fs');
+
 module.exports = {
   home: (req, res) => res.sendFile('index.html'),
-  upload: (req, res) => {
-    console.log(req.file);
-    res.json({ hello: 'world' });
+  upload: async (req, res) => {
+    try {
+      await fs.unlink(req.file.path, () =>
+        console.log(`${req.file.filename} deleted`)
+      );
+    } catch (err) {
+      console.log(`Error deleting file: ${err}`);
+    }
+    res.json({ fileSize: req.file.size });
   },
 };
